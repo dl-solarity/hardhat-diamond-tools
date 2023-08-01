d47561810a83447884d23d2475b2e513ae798f5c
Velnbur <kyrylo.baybula@distributedlab.com>
2023-08-01 23:46:26 +0300

    Revert "🔧 Make args parameter in macro optional"
    This reverts commit 429f786548b3da777f0fa30de8ed2e1ab26adbe5.

--- crates/hardhat-bindings-macro/src/lib.rs
+++ crates/hardhat-bindings-macro/src/lib.rs
@@ -1,6 +1,6 @@
 use proc_macro::Ident;
 use quote::{quote, quote_spanned, spanned::Spanned, ToTokens};
-use syn::{parse_macro_input, Path, TypePath};
+use syn::TypePath;
 
 /// Wrap a function to simplify parsing of the action arguments.
 ///
@@ -53,25 +53,12 @@ use syn::{parse_macro_input, Path, TypePath};
 /// ```
 #[proc_macro_attribute]
 pub fn hardhat_action(
-    attrs: proc_macro::TokenStream,
+    _attrs: proc_macro::TokenStream,
     item: proc_macro::TokenStream,
 ) -> proc_macro::TokenStream {
     let item = syn::parse_macro_input!(item as syn::ItemFn);
 
-    let mut no_args = false;
-    let attrs_parser = syn::meta::parser(|meta| {
-        if meta.path.is_ident("no_args") {
-            no_args = true;
-            return Ok(());
-        }
-
-        Err(syn::Error::new_spanned(meta.path, "Unknown attribute"))
-    });
-
-    // parse the `attrs` attributes using `attrs_parser`
-    syn::parse_macro_input!(attrs with attrs_parser);
-
-    match hardhat_action_impl(item, no_args) {
+    match hardhat_action_impl(item) {
         Ok(value) => value,
         Err(value) => value,
     }
@@ -80,16 +67,20 @@ pub fn hardhat_action(
 /// TODO(Velnbur): refactor this bullshit
 fn hardhat_action_impl(
     item: syn::ItemFn,
-    no_args: bool,
 ) -> Result<proc_macro::TokenStream, proc_macro::TokenStream> {
     let name = &item.sig.ident;
+
+    let action_args_type = hardhat_action_get_first_arg(&item)?;
     let block = &item.block;
+
     let return_type = hardhat_action_get_return_type(&item)?;
 
-    let args_related_block = if !no_args {
-        let action_args_type = hardhat_action_get_first_arg(&item)?;
-        quote! {
-            let _args: #action_args_type = match serde_wasm_bindgen::from_value(_args) {
+    let result_tmpl = quote! {
+        #[::wasm_bindgen::prelude::wasm_bindgen]
+        pub fn #name(args: ::wasm_bindgen::prelude::JsValue, hre: ::hardhat_bindings::bindings::runtime::HardhatRuntimeEnvironment) -> ::js_sys::Promise {
+            let hre = ::hardhat_bindings::HardhatRuntimeEnvironment::from(hre);
+
+            let args: #action_args_type = match serde_wasm_bindgen::from_value(args) {
                 Ok(args) => args,
                 Err(err) => return ::js_sys::Promise::reject(&err.into()),
             };
@@ -99,32 +90,10 @@ fn hardhat_action_impl(
             }
 
             ::wasm_bindgen_futures::future_to_promise(async move {
-                inner(_args, hre).await.map_err(|err| ::wasm_bindgen::prelude::JsValue::from(err.to_string()))?;
+                inner(args, hre).await.map_err(|err| ::wasm_bindgen::prelude::JsValue::from(err.to_string()))?;
 
                 Ok(::wasm_bindgen::prelude::JsValue::UNDEFINED)
             })
-        }
-    } else {
-        quote! {
-            async fn inner(hre: ::hardhat_bindings::HardhatRuntimeEnvironment) -> #return_type {
-                #block
-            }
-
-            ::wasm_bindgen_futures::future_to_promise(async move {
-                inner(hre).await.map_err(|err| ::wasm_bindgen::prelude::JsValue::from(err.to_string()))?;
-
-                Ok(::wasm_bindgen::prelude::JsValue::UNDEFINED)
-            })
-        }
-    };
-
-    let result_tmpl = quote! {
-        #[::wasm_bindgen::prelude::wasm_bindgen]
-        pub fn #name(_args: ::wasm_bindgen::prelude::JsValue, hre: ::hardhat_bindings::bindings::runtime::HardhatRuntimeEnvironment) -> ::js_sys::Promise {
-            let hre = ::hardhat_bindings::HardhatRuntimeEnvironment::from(hre);
-
-            #args_related_block
-
         }
     };
 
