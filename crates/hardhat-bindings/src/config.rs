dcd04512b603c47ea9e70bd3b85772fe530de1ef
Velnbur <kyrylo.baybula@distributedlab.com>
2023-08-02 00:23:36 +0300

    ✨ Add config definitions to `hardhat-bindings`

--- crates/hardhat-bindings/src/config.rs
+++ crates/hardhat-bindings/src/config.rs
@@ -1,1 +1,21 @@
+use crate::bindings;
 
+pub fn task(name: &str, description: &str) -> ConfigurableTaskDefinition {
+    bindings::config::task(name, description).into()
+}
+
+pub struct ConfigurableTaskDefinition {
+    inner: bindings::config::ConfigurableTaskDefinition,
+}
+
+impl ConfigurableTaskDefinition {
+    pub fn set_action(self, action: &bindings::config::ActionType) -> Self {
+        self.inner.set_action(action).into()
+    }
+}
+
+impl From<bindings::config::ConfigurableTaskDefinition> for ConfigurableTaskDefinition {
+    fn from(inner: bindings::config::ConfigurableTaskDefinition) -> Self {
+        Self { inner }
+    }
+}
